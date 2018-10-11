d98d36be2f067451e703174751a48f99ef52d402
android <android>
2018-10-11 15:53:15 +0800

    添加登录页面

--- pages/login/login.js
+++ pages/login/login.js
@@ -2,13 +2,13 @@ Page({
   data: {
     isShowPassword: true
   },
-  onLoad: function () {
-   
+  onLoad: function() {
+
   },
-  formSubmit: function (e) {
+  formSubmit: function(e) {
     let account = e.detail.value.account;
     let password = e.detail.value.password;
-    if (account.length === 0){
+    if (account.length === 0) {
       wx.showToast({
         title: '用户名不能为空',
         icon: 'none',
@@ -20,30 +20,22 @@ Page({
         icon: 'none',
         duration: 1500,
       })
-    }else {
-      if (account === 'admin' && e.detail.value.password === '123') {
-        wx.showLoading()
-        setTimeout(function () {
-          wx.hideLoading()
-          wx.switchTab({
-            url: '/pages/index/index',
-          })
-        }, 3000)
-      } else {
-        wx.showToast({
-          title: '用户名或密码错误',
-          icon: 'none',
-          duration: 1500,
+    } else {
+      wx.showLoading()
+      setTimeout(function() {
+        wx.hideLoading()
+        wx.switchTab({
+          url: '/pages/index/index',
         })
-      }
+      }, 3000)
     }
   },
-  switchPwd:function(e){
-    if (this.data.isShowPassword){
+  switchPwd: function(e) {
+    if (this.data.isShowPassword) {
       this.setData({
         isShowPassword: false
       })
-    }else {
+    } else {
       this.setData({
         isShowPassword: true
       })
