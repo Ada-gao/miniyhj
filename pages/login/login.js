01a4eea5b3142abde35af2af6fd32511a41397b2
android <android>
2018-10-11 15:50:06 +0800

    添加登录页面

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,23 +1,52 @@
-// pages/login/login.js
-Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-
-  },
-
-  /**
-   * 组件的初始数据
-   */
+Page({
   data: {
-
+    isShowPassword: true
   },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
+  onLoad: function () {
+   
+  },
+  formSubmit: function (e) {
+    let account = e.detail.value.account;
+    let password = e.detail.value.password;
+    if (account.length === 0){
+      wx.showToast({
+        title: '用户名不能为空',
+        icon: 'none',
+        duration: 1500,
+      })
+    } else if (password.length === 0) {
+      wx.showToast({
+        title: '密码不能为空',
+        icon: 'none',
+        duration: 1500,
+      })
+    }else {
+      if (account === 'admin' && e.detail.value.password === '123') {
+        wx.showLoading()
+        setTimeout(function () {
+          wx.hideLoading()
+          wx.switchTab({
+            url: '/pages/index/index',
+          })
+        }, 3000)
+      } else {
+        wx.showToast({
+          title: '用户名或密码错误',
+          icon: 'none',
+          duration: 1500,
+        })
+      }
+    }
+  },
+  switchPwd:function(e){
+    if (this.data.isShowPassword){
+      this.setData({
+        isShowPassword: false
+      })
+    }else {
+      this.setData({
+        isShowPassword: true
+      })
+    }
   }
 })
