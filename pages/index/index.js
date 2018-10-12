4580f7764cf3cf045ea4a9256224ef9145ed7d18
android <android>
2018-10-12 16:04:16 +0800

    添加首页任务展示

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,12 +3,34 @@ Page({
   data: {
     complete: 20, //完成的数量
     total: 120, //总数量
-    completeRate: 0,//完成率
+    completeRate: 0, //完成率
     screenWidth: 0, //屏幕宽度
-    isLoading:false,
-    isEmpty:true
+    isLoading: false,
+    isEmpty: true,
+    tasks: [{
+        id: 1,
+        name: '某某某特惠产品',
+        total: 88,
+        num: 10,
+        time: '2018.7.1'
+      },
+      {
+        id: 2,
+        name: '某某某特惠产品2',
+        total: 22,
+        num: 2,
+        time: '2018.7.2'
+      },
+      {
+        id: 3,
+        name: '某某某特惠产品3',
+        total: 33,
+        num: 3,
+        time: '2018.7.3'
+      }
+    ]
   },
-  onLoad: function () {
+  onLoad: function() {
     if (!app.globalData.token) {
       wx.redirectTo({
         url: '/pages/login/login?redirect=' + this.route + '&isTab=' + true
@@ -16,13 +38,13 @@ Page({
       return
     }
     this.setData({
-      isLoading:true,
-      isEmpty: this.data.complete>0?false:true,
+      isLoading: true,
+      isEmpty: this.data.complete > 0 ? false : true,
       completeRate: this.data.complete * 100 / this.data.total,
     })
-   
+
   },
-  openTask: function (e) {
+  openTask: function(e) {
     wx.showToast({
       title: '查看任务',
       icon: 'none',
@@ -32,7 +54,7 @@ Page({
       url: '/pages/task/task',
     })
   },
-  openCall: function (e) {
+  openCall: function(e) {
     wx.showToast({
       title: '开始外呼',
       icon: 'none',
