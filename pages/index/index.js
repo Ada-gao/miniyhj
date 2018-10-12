6d05b9d113daaa07398053f717c35c3763a56455
android <android>
2018-10-12 18:24:10 +0800

    添加任务详情页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -42,24 +42,13 @@ Page({
       isEmpty: this.data.complete > 0 ? false : true,
       completeRate: this.data.complete * 100 / this.data.total,
     })
-
   },
   openTask: function(e) {
-    wx.showToast({
-      title: '查看任务',
-      icon: 'none',
-      duration: 1500,
-    })
     wx.navigateTo({
       url: '/pages/task/task',
     })
   },
   openCall: function(e) {
-    wx.showToast({
-      title: '开始外呼',
-      icon: 'none',
-      duration: 1500,
-    })
     wx.navigateTo({
       url: '/pages/call/call',
     })
