ea8d8be18b345b3f1f59a755c87a0d3a7871c5c4
gaoyuan <gaoyuan@shuyun365.com>
2018-10-15 15:33:26 +0800

    task 页面

--- pages/task/task.js
+++ pages/task/task.js
@@ -1,18 +1,61 @@
 // pages/task/task.js
+var req = require('../../utils/request.js')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    tabs: [
+      {
+        label: '未完成',
+        value: 'dnf',
+        id: 0
+      },
+      {
+        label: '已完成',
+        value: 'finish',
+        id: 1
+      }
+    ],
+    activeIndex: 0,
+    // initDate: new Date().toLocaleDateString(),
+    initDate: '2018-10-15',
+    groupId: '',
+    listQuery: {
+      pageIndex: 0,
+      pageSize: 10,
+      type: 'dnf',
+      createTime: ''
+    },
+    date: '2016-09-01',
+    list: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options.id)
+    this.data.groupId = options.id
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+      }
+    });
+  },
+  tabClick: function (e) {
+    this.data.listQuery.type = e.currentTarget.dataset.type
+    this.setData({
+    //   sliderOffset: e.currentTarget.offsetLeft,
+      activeIndex: e.currentTarget.id
+    });
+    this.onShow()
+  },
+  bindDateChange: function (e) {
+    this.setData({
+      initDate: e.detail.value
+    })
+    this.onShow()
   },
 
   /**
@@ -26,7 +69,15 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+    var that = this
+    var data = that.data
+    var listQuery = that.data.listQuery
+    listQuery.createTime = data.initDate
+    req.get(`api/app/tasks/${data.groupId}?pageIndex=${listQuery.pageIndex}&pageSize=${listQuery.pageSize}&type=${listQuery.type}&createTime=${listQuery.createTime}`, function (res) {
+      that.setData({
+        list: res.data.content || []
+      })
+    }, false)
   },
 
   /**
--- utils/util.js
+++ utils/util.js
@@ -1,12 +1,18 @@
-const formatTime = date => {
+const formatTime = (date, dateType) => {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
   const day = date.getDate()
   const hour = date.getHours()
   const minute = date.getMinutes()
   const second = date.getSeconds()
+  let timeStr = ''
+  if (dateType === 'date') {
+    timeStr = [year, month, day].map(formatNumber).join('/')
+  } else {
+    timeStr = [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+  }
 
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+  return timeStr
 }
 
 const formatNumber = n => {
