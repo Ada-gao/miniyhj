2f57540bfafd2b7b5f05ac7b3ca77ea1cc9be454
android <android>
2018-10-19 13:08:53 +0800

    添加start页面

---
+++ pages/start/start.js
@@ -0,0 +1,40 @@
+var req = require('../../api/request.js')
+var api = require('../../api/api.js')
+const app = getApp()
+var timer
+Page({
+  onLoad: function(options) {
+    let that = this
+    timer = setTimeout(function() {
+      if (app.globalData.token) {
+        that.getUserInfo()
+      } else {
+        wx.reLaunch({
+          url: '/pages/login/login',
+        })
+      }
+    }, 1500)
+  },
+  onUnload: function() {
+    clearTimeout(timer)
+  },
+  //获取用户信息
+  getUserInfo: function() {
+    let that = this
+    req.get(api.me, function(res) {
+      //保存token信息
+      wx.setStorageSync('userInfo', JSON.stringify(res.data))
+      app.globalData.userInfo = res.data
+      wx.switchTab({
+        url: '/pages/index/index',
+      })
+    }, false)
+  },
+  //分享
+  onShareAppMessage: function() {
+    return {
+      title: '闪电呼',
+      path: '/pages/start/start'
+    }
+  }
+})
