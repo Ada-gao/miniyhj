e879b63df096227a48a16b138125f7eb529e586b
android <android>
2018-10-19 13:50:16 +0800

    add api url

---
+++ api/api.js
@@ -0,0 +1,7 @@
+const login = '/api/auth/login' //登录
+const me = '/api/app/me' //获取用户信息
+const call = '/api/app/call' //第三方呼叫
+const cancelcall = '/api/call'//取消第三方呼叫
+const tasks = '/api/app/tasks' //获取任务列表
+const getLogoAndName = '/api/app/getLogoAndName' //获取公司名称和logo
+const statisBySales = 'api/task/statisBySales'//获取任务完成状态信息
