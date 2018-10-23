c980a3677739a03a3b2fa372206639f81b975173
android <android>
2018-10-23 10:41:22 +0800

    添加upload方法

--- utils/request.js
+++ utils/request.js
@@ -4,8 +4,8 @@ let Toast = require('./Toast.js')
 let env = require('../config/env.js')
 const baseURL = env.BASE_API
 const app = getApp()
+
 function get(url, success, showLoading = true) {
-  // TODO 此方法需要优化测试
   request(url, {}, 'GET', success, showLoading)
 }
 
@@ -26,48 +26,88 @@ function request(url, data, method, success, showLoading = true) {
     wx.showLoading()
   }
   console.debug(method + ' ' + requestURL)
-  if (method === 'POST') console.debug(data)
-  let headers = {
-    'Authorization': app.globalData.token
+  if (method === 'POST') {
+    console.debug(data)
   }
   wx.request({
     url: requestURL,
     data: data,
     method: method,
-    header: headers,
-    //请求成功后调用
-    success: function(res) {
-      if (res.statusCode == 200) {
-        if (showLoading) {
-          wx.hideLoading()
-        }
-        success(res)
-      } else if (res.statusCode == 400) {
-        Toast.show(res.data.error)
-      } else if (res.statusCode == 401) {
-        // TODO 待测试
-        Toast.show('会话已过期，请重新登录。')
-        wx.redirectTo({
-          url: '/pages/login/login'
-        })
-      } else {
-        Toast.show('服务器错误')       
-      } 
+    header: {
+      'Authorization': app.globalData.token
     },
-    //请求失败后调用
-    fail:function(res){
-      Toast.show(res.errMsg)  
-    } ,
-     //请求完成后调用
-    complete: function (res) {
-     
-    }   
+    success: function(res) {
+      handleSuccess(res, success, showLoading)
+    },
+    fail: function(res) {
+      handleFail(res)
+    },
+    complete: function(res) {
+      handleComplete(res)
+    }
   })
 }
 
-module.exports = { 
-  get: get,
-  post: post,
-  put: put
+function upload(url, file, success, showLoading = true) {
+  var requestURL = url
+  if (!isAbsoluteURL(url)) {
+    requestURL = combineURLs(baseURL, url)
+  }
+  if (showLoading) {
+    wx.showLoading()
+  }
+  console.debug('upload ' + requestURL)
+  wx.uploadFile({
+    url: requestURL,
+    filePath: file,
+    name: 'file',
+    header: {
+      'Authorization': app.globalData.token
+    },
+    success: function(res) {
+      handleSuccess(res, success, showLoading)
+    },
+    fail: function(res) {
+      handleFail(res)
+    },
+    complete: function(res) {
+      handleComplete(res)
+    }
+  })
 }
 
+//请求出错
+function handleFail(res) {
+  Toast.show(res.errMsg)
+}
+
+//请求成功
+function handleSuccess(res, success, showLoading) {
+  if (res.statusCode == 200) {
+    if (showLoading) {
+      wx.hideLoading()
+    }
+    if (success) {
+      success(res)
+    }
+  } else if (res.statusCode == 400) {
+    Toast.show(res.data.error)
+  } else if (res.statusCode == 401) {
+    Toast.show('会话已过期，请重新登录。')
+    wx.reLaunch({
+      url: '/pages/login/login'
+    })
+  } else {
+    Toast.show('服务器错误')
+  }
+}
+
+//请求完成
+function handleComplete(res) {}
+
+module.exports = {
+  get: get,
+  post: post,
+  put: put,
+  upload: upload,
+}
