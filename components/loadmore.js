514fc6bcfbdd1f5a2f3f1f5c5bf62040d025e2b0
android <android>
2018-11-22 17:39:03 +0800

    抽取加载更多组件

---
+++ components/loadmore.js
@@ -0,0 +1,18 @@
+Component({
+  properties: {
+    hasMore: {
+      type: Boolean,
+      value: true
+    },
+    size: {
+      type: Number,
+      value: 0
+    }
+  },
+  data: {
+
+  },
+  methods: {
+
+  }
+})
