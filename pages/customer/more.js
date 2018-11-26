5a513b1c98ddd1bca9d7efa97a6e05787cf780ba
android <android>
2018-11-26 09:54:47 +0800

    添加搜索更多页面

---
+++ pages/customer/more.js
@@ -0,0 +1,62 @@
+var req = require('../../utils/request.js')
+var common = require('../../common/common.js')
+const app = getApp()
+Page({
+  data: {
+    type: '',
+    keyword:'',
+    pageIndex: 0,
+    hasMore: true,
+    list:''
+  },
+  onLoad: function (options) {
+    this.setData({
+      type: options.type,
+      keyword: options.keyword
+    })
+    this.getData()
+  },
+  getData: function () {
+    let that = this
+    req.get('task/getPotentialUser?pageSize=20&type='+that.data.type+'&name=' + that.data.keyword + '&pageIndex=' + that.data.pageIndex, function (res) {
+      let model = res.data
+      let hasMore = true
+      if (that.data.pageIndex >= model.totalPages - 1) {
+        hasMore = false
+      }
+      if (that.data.pageIndex > 0) {
+        model.content = that.data.tasks.concat(model.content)
+      }
+      that.setData({
+        list: model.content,
+        hasMore: hasMore,
+        pageIndex: that.data.pageIndex + 1,
+      })
+    }, true, function () {
+      wx.stopPullDownRefresh();
+    })
+  },
+  openTask: function (e) {
+    let taskId = e.currentTarget.dataset.taskid
+    wx.navigateTo({
+      url: '/pages/call/call?taskId=' + taskId,
+    })
+  },
+  onPullDownRefresh: function () {
+    this.setData({
+      pageIndex: 0,
+    })
+    this.getData()
+  },
+  onReachBottom: function () {
+    if (this.data.hasMore) {
+      this.getData()
+    }
+  },
+  onShareAppMessage: function () {
+    return common.onShareAppMessage()
+  },
+  back: function () {
+    common.back()
+  }
+})
--- pages/customer/search.js
+++ pages/customer/search.js
@@ -4,32 +4,41 @@ Page({
   data: {
     followList: '',
     starList: '',
+    keyword: '',
+    hasFollow: false,
+    hasStar: false,
   },
   search: function(e) {
     let keyword = e.detail.value
     if (keyword) {
-      this.getFollows(keyword)
-      this.getStars(keyword)
+      this.setData({
+        keyword: keyword
+      })
+      this.getFollows()
+      this.getStars()
     } else {
       this.setData({
         followList: '',
-        starList: ''
+        starList: '',
+        keyword: ''
       })
     }
   },
-  getFollows: function(name) {
+  getFollows: function() {
     let that = this
-    req.get('task/getPotentialUser?pageSize=20&type=follow&name=' + name, function(res) {
+    req.get('task/getPotentialUser?pageSize=5&type=follow&name=' + that.data.keyword, function(res) {
       that.setData({
         followList: res.data.content,
+        hasFollow: res.data.totalPages > 1
       })
     })
   },
-  getStars: function(name) {
+  getStars: function() {
     let that = this
-    req.get('task/getPotentialUser?pageSize=20&type=star&name=' + name, function(res) {
+    req.get('task/getPotentialUser?pageSize=5&type=star&name=' + that.data.keyword, function(res) {
       that.setData({
         starList: res.data.content,
+        hasStar: res.data.totalPages > 1
       })
     })
   },
@@ -37,6 +46,12 @@ Page({
     let taskId = e.currentTarget.dataset.taskid
     wx.navigateTo({
       url: '/pages/call/call?taskId=' + taskId,
+    })
+  },
+  openMore: function(e) {
+    let type = e.currentTarget.dataset.type
+    wx.navigateTo({
+      url: '/pages/customer/more?type=' + type + '&keyword=' + this.data.keyword,
     })
   },
   onShareAppMessage: function() {
